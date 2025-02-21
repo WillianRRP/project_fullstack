85a5339116986312efb7c7b37853c9cf44be4992
WillianRRP <willianrenato754@gmail.com>
2025-02-21 19:11:07 -0300

    Creating the BMI Calculator

---
+++ Calculadora Imc/script.js
@@ -0,0 +1,36 @@
+var peso;
+var altura;
+var imc;
+var resultado;
+
+
+function calcular(event){  
+    event.preventDefault()
+
+
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+
+
+    imc = peso / (altura * altura);
+    resultado = document.getElementById('resultado');
+
+    if(imc < 17){
+
+        resultado.innerHTML = "<br/> Seu Resultado foi: " + imc.toFixed(2) + '<br/> Cuidado vc está muito abaixo do peso' 
+    }else if(imc > 17 && imc <= 18.49){
+        resultado.innerHTML = "<br/> Seu Resultado foi: " + imc.toFixed(2) + '<br/> Cuidado vc está abaixo do peso' 
+    }else if(imc >= 18.5 && imc <= 24.99){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = "<br/> Seu Resultado foi: " + imc.toFixed(2) + '<br/> Vc está no peso ideal' 
+    }else if(imc >= 25 && imc <= 29.99){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = "<br/> Seu Resultado foi: " + imc.toFixed(2) + '<br/> Cuidado vc está acima do peso' 
+    }else if(imc >= 30){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = "<br/> Seu Resultado foi: " + imc.toFixed(2) + '<br/> Cuidado com a obesidade' 
+    }
+    document.getElementById('peso').value = '';
+    document.getElementById('altura').value = '';
+
+}
